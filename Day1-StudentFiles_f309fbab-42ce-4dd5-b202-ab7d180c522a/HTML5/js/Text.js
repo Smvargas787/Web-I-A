8224750a2da2d1e117465a53f7795ae4a54f047a
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2015-09-29 19:25:15 -0400

    day 1 lessons done

--- Day1-StudentFiles_f309fbab-42ce-4dd5-b202-ab7d180c522a/HTML5/js/Text.js
+++ Day1-StudentFiles_f309fbab-42ce-4dd5-b202-ab7d180c522a/HTML5/js/Text.js
@@ -4,8 +4,18 @@ window.onload = function() {
 	var ctx = theCanvas.getContext("2d");
 	if (ctx) {
 						
-						
-						
+				var theString = "Drawing Text on Canvas";
+
+		//draw simple text
+		ctx.fillText(theString, 20, 20);
+
+		//draw text with font info
+		ctx.font = "25pt Georgia";
+		ctx.fillText(theString, 20, 60);
+
+		//add some color
+		ctx.fillStyle = "blue";
+		ctx.fillText(theString, 20, 100);
 						
 						
 		}
